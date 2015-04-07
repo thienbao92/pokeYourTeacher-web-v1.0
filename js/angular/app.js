497697a26cc18efec206361d03dd5ee83cf410b3
thienbao92 <thienbao.vudao@yahoo.com>
2015-04-07 11:14:01 +0300

    add ui.router

---
+++ js/angular/app.js
@@ -0,0 +1,1 @@
+var myApp = angular.module('myApp', ['myApp.controller','ui.router']);
---
+++ js/angular/controller.js
@@ -0,0 +1,5 @@
+angular.module('myApp.controller', []).
+
+controller('MainCtrl', ['$scope', function ($scope) {
+	console.log("loaded successfully");
+}])
